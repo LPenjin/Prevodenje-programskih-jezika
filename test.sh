e7c62ac7288a380ec567150e602f122315aa4516
Tin Plavec <tin.plavec@mindsmiths.com>
2021-10-28 21:13:07 +0200

    added bash script for testing

---
+++ test.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+LAB=Lab1
+
+cd ${LAB}
+
+for DIR in $(ls -d test_primjeri/*); do
+  cat ${DIR}/test.lan | python3 GLA.py 1>/dev/null && OUTPUT="$(cat ${DIR}/test.in | python3 analizator/LA.py)"
+  DIFF="$( diff ${DIR}/test.out <(echo "$OUTPUT") )"
+  if [[ ${DIFF} != "" ]]; then
+    echo "${DIR}"
+    echo "${DIFF}"
+    exit 1;
+  else
+    echo "${DIR} is fine."
+  fi
+done
